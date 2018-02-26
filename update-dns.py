e90bd8be74bcf7aa1cd5dacd4668c894afb05850
Uli Hitzel <uli.hitzel@gmail.com>
2018-02-26 23:00:10 +0000

    changed

--- update-dns.py
+++ update-dns.py
@@ -1,5 +1,6 @@
-import requests
+import requests, sys
 import random, string
+import base64, datetime
 
 # Read Config
 execfile("hover.cfg")
@@ -7,13 +8,20 @@ execfile("hover.cfg")
 # API Endpoint
 url = "https://www.hover.com/api/login"
 
-# Generate random string
-len=255
+# Generate payload
+
+d=str(datetime.datetime.now())
+len=64
 rand=""
 
 for _ in range(len):
 	rand += random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
 
+
+p1 = {"timestamp":d, "key": rand}
+payload_data = base64.urlsafe_b64encode(str(p1))
+
+# sys.exit(0)
 # Login
 payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\n"+username+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n"+password+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
 headers = {
@@ -32,7 +40,7 @@ else:
 	# The DNS entry we want to update
 	url = "https://www.hover.com/api/dns/dns16451432"
 	
-	payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n"+str(rand)+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+	payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n"+str(payload_data)+"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
 	headers = {
 	    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
 	    'Cookie': "hoverauth="+response.cookies["hoverauth"],
